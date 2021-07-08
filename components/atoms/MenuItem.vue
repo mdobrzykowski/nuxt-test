6d63d00e59d32ed5b69fc3ddf258443fd7c16c10
Michał Dobrzykowski <82824650+mdobrzykowski@users.noreply.github.com>
2021-07-08 15:26:23 +0200

    Create app template

---
+++ components/atoms/MenuItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-list-item
+        :to="to"
+        router
+        exact
+    >
+        <v-list-item-action>
+            <v-icon>{{ icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+            <v-list-item-title v-text="title" />
+        </v-list-item-content>
+    </v-list-item>
+</template>
+
+<script>
+export default {
+    props: [
+        'to',
+        'icon',
+        'title',
+    ],
+}
+</script>
